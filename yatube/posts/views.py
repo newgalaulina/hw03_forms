f0e43ce277f014eb5b99406a984a4e74cb8f6e50
Платон <zhemerov17@mail.ru>
2022-08-11 19:07:46 +0300

    last commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Post, Group, User
 
-from .forms import PostForm 
+from .forms import PostForm
 
-from .utils import get_page_context 
+from .utils import get_page_context
 
 
 def index(request):
